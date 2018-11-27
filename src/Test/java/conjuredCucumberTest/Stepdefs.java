52cd9fec333072f5788b5683cad960b4ae79e432
Nathan <nathanamar19@gmail.com>
2018-11-27 18:02:18 +0100

    cucumber test 1.0

--- src/Test/java/conjuredCucumberTest/Stepdefs.java
+++ src/Test/java/conjuredCucumberTest/Stepdefs.java
@@ -5,41 +5,35 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import edu.insightr.gildedrose.Inventory;
-import edu.insightr.gildedrose.Item;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
-//import static  org.junit.Assert.assertThat;
-//import static org.hamcrest.CoreMatchers.is;
-
+import static org.junit.Assert.assertEquals;
 
 public class Stepdefs {
+
     private Inventory inventory;
-    private Item[] items;
-    private Item vest;
-    private Item conjured;
 
-    @Given("^I have a new inventory$")
-    public void iHaveANewInventory() throws Throwable {
-        inventory = new Inventory();
-        items = inventory.getItems();
-        conjured = items[5];
+    @Given("I have a new inventory")
+    public void i_have_a_new_inventory()
+    {
+        inventory =  new Inventory();
     }
 
-    @Then("^the quality of the conjured is (\\d+)$")
-    public void theQualityOfTheConjuredIs(int conjuredQuality) throws Throwable {
-        assertThat(conjured.getQuality(), is(conjuredQuality));
+    @Then("the quality of the conjured item is 6")
+    public void the_quality_of_the_conjured_item_is_6()
+    {
+        assertEquals(6,inventory.getItems()[5].getQuality());
     }
 
-    @When("^I update the inventory$")
-    public void iUpdateTheInventory() throws Throwable {
+    @When("I update the inventory")
+    public void i_update_the_inventory()
+    {
         inventory.updateQuality();
     }
 
-    @Then("^the quality of the conjured item is (\\d+)$")
-    public void theQualityOfTheConjuredItemIs(int conjuredQuality) throws Throwable {
-        assertThat(conjured.getQuality(), is(conjuredQuality));
+    @Then("the quality of the conjured item is 4")
+    public void the_quality_of_the_conjured_item_is_4()
+    {
+        assertEquals(5,inventory.getItems()[5].getQuality());
     }
-}
 
+}
