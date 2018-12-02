810faa5d1b81fbd1901f2e7ee3ee2378861208cf
IlyessAgg <ilyessagg@gmail.com>
2018-12-02 14:09:43 +0100

    Control Commmit.

--- src/main/java/edu/insightr/gildedrose/Control.java
+++ src/main/java/edu/insightr/gildedrose/Control.java
@@ -1,5 +1,13 @@
 package edu.insightr.gildedrose;
 
+import javafx.event.ActionEvent;
+
 public class Control {
     //ici nous allons faire le lien avec notre view
+
+    public void addItem(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void updateItem(ActionEvent actionEvent) {
+    }
 }
