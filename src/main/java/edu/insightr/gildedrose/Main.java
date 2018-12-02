e3467afc8661aa30d20e71f930afe30c0c26bd55
IlyessAgg <ilyessagg@gmail.com>
2018-12-02 14:11:42 +0100

    Main of the Application.

---
+++ src/main/java/edu/insightr/gildedrose/Main.java
@@ -0,0 +1,21 @@
+package edu.insightr.gildedrose;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view.fxml"));
+        primaryStage.setTitle("Item List");
+        primaryStage.setScene(new Scene(root, 581, 500));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
